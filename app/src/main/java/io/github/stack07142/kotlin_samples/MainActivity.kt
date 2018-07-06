2da29d9586ced0e53f8563e5318ccf31acbd04b2
stack07142 <stack.07142@gmail.com>
2018-07-06 15:45:23 +0900

    gradle

--- app/src/main/java/io/github/stack07142/kotlin_samples/MainActivity.kt
+++ app/src/main/java/io/github/stack07142/kotlin_samples/MainActivity.kt
@@ -1,7 +1,9 @@
 package io.github.stack07142.kotlin_samples
 
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +16,15 @@ class MainActivity : AppCompatActivity() {
                     .replace(android.R.id.content, MainFragment())
                     .commit()
         }
+
+        /*
+        val links: MutableMap<String, String> = mutableMapOf()
+        links["a"] = "b"
+        links["b"] = "c"
+        links["c"] = "d"
+
+        val answer = links.getOrDefault("d", "deffff")
+        Timber.d("answer= $answer")
+        */
     }
 }

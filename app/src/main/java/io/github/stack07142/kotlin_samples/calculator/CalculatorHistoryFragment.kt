22453c3dba85f388106456128984dbe28303e25d
stack07142 <stack.07142@gmail.com>
2018-07-10 13:36:12 +0900

    calculator-history

--- app/src/main/java/io/github/stack07142/kotlin_samples/calculator/CalculatorHistoryFragment.kt
+++ app/src/main/java/io/github/stack07142/kotlin_samples/calculator/CalculatorHistoryFragment.kt
@@ -1,12 +1,15 @@
 package io.github.stack07142.kotlin_samples.calculator
 
 import android.app.Fragment
+import android.content.Context
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import io.github.stack07142.kotlin_samples.R
-import timber.log.Timber
+import kotlinx.android.synthetic.main.fragment_calculator_history.*
 
 class CalculatorHistoryFragment : Fragment() {
     companion object {
@@ -31,6 +34,24 @@ class CalculatorHistoryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         history = arguments.getStringArrayList(ARG_HISTORY)
-        Timber.d(history.toString())
+        rv_calc_history.adapter = HistoryRVAdapter(activity, history)
+    }
+
+    class HistoryRVAdapter(val context: Context, val history: List<String>?): RecyclerView.Adapter<HistoryItemVH>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryItemVH {
+            return HistoryItemVH(LayoutInflater.from(context).inflate(R.layout.item_history, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: HistoryItemVH, position: Int) {
+            holder.textView.text = history?.get(position)
+        }
+
+        override fun getItemCount(): Int {
+            return history?.size ?: 0
+        }
+    }
+
+    class HistoryItemVH(view: View) : RecyclerView.ViewHolder(view) {
+        val textView = view as TextView
     }
 }
